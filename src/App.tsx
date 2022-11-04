61445fb9269e01fe83507960002b3ed011c9c49b
KauanR <kau.matosr@gmail.com>
2022-11-04 20:04:44 -0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import './App.scss'
+
+function App() {
+    return (
+        <Typography variant='h1'>
+            Flood Detection
+        </Typography>
+    )
+}
+
+export default App
